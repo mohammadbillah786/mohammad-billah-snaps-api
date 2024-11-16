2476645f7a1643a58c0b5a7362b3f80fc025a52a
Mohammad Billah <mobillah1@gmail.com>
2024-11-16 17:47:18 -0500

    Done API

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
 
 import tags from './routes/tags.js';
 import photos from './routes/photos.js';
@@ -14,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('public/images'));
+app.use(express.static('./public'));
 
 app.use('/tags', tags);   
 app.use('/photos', photos); 
